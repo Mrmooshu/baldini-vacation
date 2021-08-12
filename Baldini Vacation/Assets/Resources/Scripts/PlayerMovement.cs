126c8cb6debb174b9c6a487a25e1f92f40f0328f
Mrmooshu <mrmooshu3@hotmail.com>
2021-08-12 16:04:16 -0400

    basic character controls

---
+++ Baldini Vacation/Assets/Resources/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Controller controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale > 0)
+        {
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+            //animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+                animator.SetBool("Jumping", true);
+            }
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+}
