fd0348107a35361eaf421113b4b898217dca7fe5
John <iamgrumby@gmail.com>
2021-08-15 14:20:36 -0500

    newScript+Wallslide

--- Baldini Vacation/Assets/Resources/Scripts/PlayerMovement.cs
+++ Baldini Vacation/Assets/Resources/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement :  MonoBehaviour
 {
     public Controller controller;
     public Animator animator;
