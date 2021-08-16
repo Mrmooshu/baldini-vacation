25053ba7cfa5286da173e5b3cec08b3574d74d8f
John <iamgrumby@gmail.com>
2021-08-16 06:58:41 -0500

    Dash, attack1

--- Baldini Vacation/Assets/Resources/Scripts/newPlayerController.cs
+++ Baldini Vacation/Assets/Resources/Scripts/newPlayerController.cs
@@ -9,6 +9,9 @@ public class newPlayerController : MonoBehaviour
     private float jumpTimer;
     private float turnTimer;
     private float wallJumpTimer;
+    private float dashTimeLeft;
+    private float lastImageXpos;
+    private float lastDash = -100f;
 
     private int amountOfJumpsLeft;
     private int facingDirection = 1;
@@ -26,6 +29,7 @@ public class newPlayerController : MonoBehaviour
     private bool canMove;
     private bool canFlip;
     private bool hasWallJumped;
+    private bool isDashing;
 
     private Rigidbody2D rb;
     private Animator anim;
@@ -43,6 +47,10 @@ public class newPlayerController : MonoBehaviour
     public float jumpTimerSet = 0.15f;
     public float turnTimerSet = 0.01f;
     public float wallJumpTimerSet = 0.5f;
+    public float dashTime;
+    public float dashSpeed;
+    public float distanceBetweenImages;
+    public float dashCoolDown;
 
     public Vector2 wallHopDirection;
     public Vector2 wallJumpDirection;
@@ -71,6 +79,7 @@ public class newPlayerController : MonoBehaviour
         CheckIfCanJump();
         CheckIfWallSliding();
         CheckJump();
+        CheckDash();
     }
 
     private void FixedUpdate()
@@ -130,7 +139,7 @@ public class newPlayerController : MonoBehaviour
             Flip();
         }
 
-        if (rb.velocity.x > 0.01f || rb.velocity.x < -0.01f)
+        if (Mathf.Abs(rb.velocity.x) >= 0.01f)
         {
             isRunning = true;
         }
@@ -192,10 +201,53 @@ public class newPlayerController : MonoBehaviour
         {
             checkJumpMultiplier = false;
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
-        }    
+        }  
+        
+        if(Input.GetButtonDown("Dash"))
+        {
+            if (Time.time >= (lastDash + dashCoolDown))
+                AttemptToDash();
+        }
 
     }
 
+    private void AttemptToDash()
+    {
+        isDashing = true;
+        dashTimeLeft = dashTime;
+        lastDash = Time.time;
+
+        PlayerAfterImagePool.Instance.GetFromPool();
+        lastImageXpos = transform.position.x;
+    }    
+
+    private void CheckDash()
+    {
+        if(isDashing)
+        {
+            if(dashTimeLeft > 0)
+            {
+                canMove = false;
+                canFlip = false;
+                rb.velocity = new Vector2(dashSpeed * facingDirection, rb.velocity.y);
+                dashTimeLeft -= Time.deltaTime;
+
+                if (Mathf.Abs(transform.position.x - lastImageXpos) > distanceBetweenImages)
+                {
+                    PlayerAfterImagePool.Instance.GetFromPool();
+                    lastImageXpos = transform.position.x;
+                }
+            }
+
+            if(dashTimeLeft <= 0 || isTouchingWall)
+            {
+                isDashing = false;
+                canMove = true;
+                canFlip = true;
+            }
+          
+        }
+    }
     private void CheckJump()
     {
         if(jumpTimer > 0)
@@ -293,6 +345,16 @@ public class newPlayerController : MonoBehaviour
         }
     }
 
+    public void DisableFlip()
+    {
+        canFlip = false;
+    }
+
+    public void EnableFlip()
+    {
+        canFlip = true;
+    }
+
     private void Flip()
     {
         if (!isWallSliding && canFlip)
@@ -303,7 +365,7 @@ public class newPlayerController : MonoBehaviour
         }
         }
 
-    private void OnDrawGismos()
+    private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
 
